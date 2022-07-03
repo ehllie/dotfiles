1b9110804de224ee4ba564822fe6fb4d6851c22d
Elizabeth <me+github@ehllie.xyz>
2022-07-03 15:13:53 +0200

    Using explicit commands instead of aliases in the boostrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,7 @@ fi
 
 git clone "--separate-git-dir=${XDG_CONFIG_HOME:-$HOME/.config}/dotfile-gitdir" "https://github.com/ehllie/my-dotfiles.git" "${XDG_CACHE_HOME:-$HOME/.cache}/dotfile-gitdir"
 rm -r "${XDG_CACHE_HOME:-$HOME/.cache}/dotfile-gitdir"
-alias config='git --git-dir=${XDG_CONFIG_HOME:-$HOME/.config}/dotfile-gitdir --work-tree=$HOME'
-config update-index --assume-unchanged $HOME/{README.md,bootstrap.sh}
-config config status.showUntrackedFiles no
-config status
-echo "Force sync with remote? y/n"
-read -r force
-if [ "$force" == "y" ]; then
-    config pull
-fi
+git --git-dir=${XDG_CONFIG_HOME:-$HOME/.config}/dotfile-gitdir --work-tree=$HOME update-index --assume-unchanged $HOME/{README.md,bootstrap.sh}
+git --git-dir=${XDG_CONFIG_HOME:-$HOME/.config}/dotfile-gitdir --work-tree=$HOME config status.showUntrackedFiles no
+git --git-dir=${XDG_CONFIG_HOME:-$HOME/.config}/dotfile-gitdir --work-tree=$HOME status
 rm $HOME/{README.md,bootstrap.sh}
