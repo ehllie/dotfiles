d8e63c6741cc6790bcbfe84894911d39fda40a79
Elizabeth Paź <me+github@ehllie.xyz>
2022-07-03 17:38:14 +0200

    Added nvim config back into the repo

---
+++ .config/nvim/lua/user/lsp/settings/pyright.lua
@@ -0,0 +1,10 @@
+return {
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "strict",
+        extraPaths = { ".local/lib/python3.10/site-packages" },
+      },
+    },
+  },
+}
