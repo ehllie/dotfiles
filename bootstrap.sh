fb9129f44055ff0e3c5c0012a291ea24bfe179c2
Elizabeth <me+github@ehllie.xyz>
2022-07-03 14:57:51 +0200

    Changed ~ to /home/ellie while parsing XDG directories

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,8 @@ if [ "$(id -u)" == "0" ]; then
     exit 1
 fi
 
-git clone "--separate-git-dir=${XDG_CONFIG_HOME:-~/.config}/dotfile-gitdir" "git@github.com:ehllie/my-dotfiles.git" "${XDG_CACHE_HOME:-~/.cache}/dotfile-gitdir"
-rm -r "${XDG_CACHE_HOME:-~/.cache}/dotfiles"
+git clone "--separate-git-dir=${XDG_CONFIG_HOME:-$HOME/.config}/dotfile-gitdir" "git@github.com:ehllie/my-dotfiles.git" "${XDG_CACHE_HOME:-$HOME/.cache}/dotfile-gitdir"
+rm -r "${XDG_CACHE_HOME:-$HOME/.cache}/dotfiles"
 alias config='git --git-dir=${XDG_CONFIG_HOME:-~/.config}/dotfile-gitdir --work-tree=$HOME'
 config update-index --assume-unchanged $HOME/{README.md,bootstrap.sh}
 config config status.showUntrackedFiles no
