4145360f3b305efb17f21fbd0cd523e9471dcd53
Elizabeth Paź <me+github@ehllie.xyz>
2022-07-10 21:43:31 +0200

    Added rust-tools config

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -92,6 +92,7 @@ return packer.startup(function(use)
   use({ "williamboman/nvim-lsp-installer" }) -- simple to use language server installer
   use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
   use({ "RRethy/vim-illuminate" })
+  use({ "simrat39/rust-tools.nvim" })
 
   -- Telescope
   use({ "nvim-telescope/telescope.nvim" })
