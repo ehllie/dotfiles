822a65ad26a4507db6eae26bcdfa1b8656d43ddc
Elizabeth Paź <me+github@ehllie.xyz>
2022-07-17 02:29:34 +0200

    Rewrote the lsp-installer logic

--- .config/nvim/lua/user/lsp/lsp-installer.lua
+++ .config/nvim/lua/user/lsp/lsp-installer.lua
@@ -31,81 +31,17 @@ for _, server in pairs(servers) do
     on_attach = require("user.lsp.handlers").on_attach,
     capabilities = require("user.lsp.handlers").capabilities,
   }
-  local do_setup = true
 
-  if server == "sumneko_lua" then
-    local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+
+  local opts_ok, server_opts = pcall(require, "user.lsp.settings." .. server)
+  if opts_ok then
+    local opt_type = type(server_opts)
+    if opt_type == "table" then
+      opts = vim.tbl_deep_extend("force", server_opts, opts)
+      lspconfig[server].setup(opts)
+    elseif opt_type == "function" then
+      server_opts(lspconfig, opts)
+    end
   end
 
-  if server == "pylsp" then
-    do_setup = false
-    local pylsp_opts = require("user.lsp.settings.pylsp")
-    opts = vim.tbl_deep_extend("force", pylsp_opts, opts)
-  end
-
-  if server == "pyright" then
-    local pyright_opts = require("user.lsp.settings.pyright")
-    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-  end
-
-  if server == "rust_analyzer" then
-    local keymap = vim.keymap.set
-    local key_opts = { silent = true }
-
-    keymap("n", "<leader>rh", "<cmd>RustSetInlayHints<Cr>", key_opts)
-    keymap("n", "<leader>rhd", "<cmd>RustDisableInlayHints<Cr>", key_opts)
-    keymap("n", "<leader>th", "<cmd>RustToggleInlayHints<Cr>", key_opts)
-    keymap("n", "<leader>rr", "<cmd>RustRunnables<Cr>", key_opts)
-    keymap("n", "<leader>rem", "<cmd>RustExpandMacro<Cr>", key_opts)
-    keymap("n", "<leader>roc", "<cmd>RustOpenCargo<Cr>", key_opts)
-    keymap("n", "<leader>rpm", "<cmd>RustParentModule<Cr>", key_opts)
-    keymap("n", "<leader>rjl", "<cmd>RustJoinLines<Cr>", key_opts)
-    keymap("n", "<leader>rha", "<cmd>RustHoverActions<Cr>", key_opts)
-    keymap("n", "<leader>rhr", "<cmd>RustHoverRange<Cr>", key_opts)
-    keymap("n", "<leader>rmd", "<cmd>RustMoveItemDown<Cr>", key_opts)
-    keymap("n", "<leader>rmu", "<cmd>RustMoveItemUp<Cr>", key_opts)
-    keymap("n", "<leader>rsb", "<cmd>RustStartStandaloneServerForBuffer<Cr>", key_opts)
-    keymap("n", "<leader>rd", "<cmd>RustDebuggables<Cr>", key_opts)
-    keymap("n", "<leader>rv", "<cmd>RustViewCrateGraph<Cr>", key_opts)
-    keymap("n", "<leader>rw", "<cmd>RustReloadWorkspace<Cr>", key_opts)
-    keymap("n", "<leader>rss", "<cmd>RustSSR<Cr>", key_opts)
-    keymap("n", "<leader>rxd", "<cmd>RustOpenExternalDocs<Cr>", key_opts)
-
-    require("rust-tools").setup({
-      tools = {
-        on_initialized = function()
-          vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave", "BufWritePost" }, {
-            callback = vim.lsp.codelens.refresh,
-            pattern = "*.rs",
-          })
-        end,
-      },
-      server = {
-        on_attach = opts.on_attach,
-        capabilities = opts.capabilities,
-        settings = {
-          ["rust-analyzer"] = {
-            lens = {
-              enable = true,
-            },
-            checkOnSave = {
-              command = "clippy",
-            },
-          },
-        },
-      },
-    })
-
-    do_setup = false
-  end
-
-  if server == "volar" then
-    local volar_opts = require("user.lsp.settings.volar")
-    opts = vim.tbl_deep_extend("force", volar_opts, opts)
-  end
-
-  if do_setup then
-    lspconfig[server].setup(opts)
-  end
 end
