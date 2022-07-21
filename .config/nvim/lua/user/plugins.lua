60591b8d482a2cd160db23d050e321f2bcc3b595
Elizabeth Paź <me@ehllie.xyz>
2022-07-21 15:21:13 +0200

    Enabled packer profiling

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -40,59 +40,62 @@ packer.init({
 })
 
 -- Install your plugins here
-return packer.startup(function(use)
-  use({ "wbthomason/packer.nvim" }) -- Have packer manage itself
+return packer.startup({
+  function(use)
+    use({ "wbthomason/packer.nvim" }) -- Have packer manage itself
 
-  use({
-    "kyazdani42/nvim-web-devicons",
-    {
-      "lambdalisue/suda.vim",
-      config = function()
-        require("which-key").register({ ["w!!"] = { "SudaWrite", "Write as superuser" } }, { mode = "c" })
-      end,
-      requires = "folke/which-key.nvim",
-    },
-    "michaeljsmith/vim-indent-object",
-    "moll/vim-bbye",
-    "nvim-lua/plenary.nvim", -- Useful lua functions used by lots of plugins
-    "tpope/vim-surround",
-  })
+    use({
+      "kyazdani42/nvim-web-devicons",
+      {
+        "lambdalisue/suda.vim",
+        config = function()
+          require("which-key").register({ ["w!!"] = { "SudaWrite", "Write as superuser" } }, { mode = "c" })
+        end,
+        requires = "folke/which-key.nvim",
+      },
+      "michaeljsmith/vim-indent-object",
+      "moll/vim-bbye",
+      "nvim-lua/plenary.nvim", -- Useful lua functions used by lots of plugins
+      "tpope/vim-surround",
+    })
 
-  -- TODO: Configure symbols-outline
-  use({ "simrat39/symbols-outline.nvim" })
+    -- TODO: Configure symbols-outline
+    use({ "simrat39/symbols-outline.nvim" })
 
-  -- LSP
-  -- use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
-  -- use({ "neovim/nvim-lspconfig" }) -- enable LSP
-  -- use({ "simrat39/rust-tools.nvim" })
-  -- use({ "williamboman/nvim-lsp-installer" }) -- simple to use language server installer
-  -- use({ "folke/lua-dev.nvim" })
+    -- LSP
+    -- use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
+    -- use({ "neovim/nvim-lspconfig" }) -- enable LSP
+    -- use({ "simrat39/rust-tools.nvim" })
+    -- use({ "williamboman/nvim-lsp-installer" }) -- simple to use language server installer
+    -- use({ "folke/lua-dev.nvim" })
 
-  -- use({ "github/copilot.vim" }) -- Uncomment to setup copilot
+    -- use({ "github/copilot.vim" }) -- Uncomment to setup copilot
 
-  -- Depencancy and config setup for plugins in separate files
-  use(require("user.alpha"))
-  use(require("user.bufferline"))
-  use(require("user.cmp"))
-  use(require("user.comment"))
-  use(require("user.dap"))
-  use(require("user.formatter"))
-  use(require("user.hop"))
-  use(require("user.lsp"))
-  use(require("user.lualine"))
-  use(require("user.misc"))
-  use(require("user.nvim-tree"))
-  use(require("user.presence"))
-  use(require("user.telescope"))
-  use(require("user.toggleterm"))
-  use(require("user.treesitter"))
-  -- TODO: Load keybinds declared in a global table at the end of init
-  -- That way each plugin that sets up keybibds won't depend on which-key
-  use(require("user.which-key"))
+    -- Depencancy and config setup for plugins in separate files
+    use(require("user.alpha"))
+    use(require("user.bufferline"))
+    use(require("user.cmp"))
+    use(require("user.comment"))
+    use(require("user.dap"))
+    use(require("user.formatter"))
+    use(require("user.hop"))
+    use(require("user.lsp"))
+    use(require("user.lualine"))
+    use(require("user.misc"))
+    use(require("user.nvim-tree"))
+    use(require("user.presence"))
+    use(require("user.telescope"))
+    use(require("user.toggleterm"))
+    use(require("user.treesitter"))
+    -- TODO: Load keybinds declared in a global table at the end of init
+    -- That way each plugin that sets up keybibds won't depend on which-key
+    use(require("user.which-key"))
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if PACKER_BOOTSTRAP then
+      require("packer").sync()
+    end
+  end,
+  config = { profile = { enable = true } },
+})
