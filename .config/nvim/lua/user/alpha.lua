f24fdb21394acc5c84a8c066f377d330489405a9
Elizabeth Paź <me@ehllie.xyz>
2022-07-21 16:01:33 +0200

    Centered alpha footer

--- .config/nvim/lua/user/alpha.lua
+++ .config/nvim/lua/user/alpha.lua
@@ -20,12 +20,44 @@ local function config()
     dashboard.button("p", "勒 " .. " Update plugins", ":PackerSync<CR>"),
     dashboard.button("q", " " .. " Quit", ":qa<CR>"),
   }
+
+  ---@generic T
+  ---@param str `T`
+  ---@return `T`
+  local function id(str)
+    return str
+  end
+
+  ---@param vals table
+  ---@return fun(str: string): string)
+  local function format(vals)
+    ---@param str string
+    return function(str)
+      return string.format(str, unpack(vals))
+    end
+  end
+
+  ---@generic R
+  ---@param funcs table<fun(arg: `T`): `R`>
+  ---@param args table<`T`>
+  ---@return table<`R`>
+  local function zip_map(funcs, args)
+    local results = {}
+    for i, func in ipairs(funcs) do
+      results[i] = func(args[i])
+    end
+    return results
+  end
+
   local function footer()
     local plugin_count = 0
     for _, _ in pairs(packer_plugins) do
       plugin_count = plugin_count + 1
     end
-    return string.format("%d plugins installed\nNeovim is pretty cool actually", plugin_count)
+    return zip_map({ format({ plugin_count }), id }, {
+      "     %d plugins installed     ",
+      "Neovim is pretty cool actually",
+    })
   end
 
   dashboard.section.footer.val = footer()
