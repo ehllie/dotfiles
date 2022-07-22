90d70bc4854c330944928b1fb147f03261ce892f
Elizabeth Paź <me@ehllie.xyz>
2022-07-22 10:52:21 +0200

    Made alpha prettier and more custom

--- .config/nvim/lua/plug-conf/alpha.lua
+++ .config/nvim/lua/plug-conf/alpha.lua
@@ -1,12 +1,30 @@
 local function config()
   local dashboard = require("alpha.themes.dashboard")
+  local fortune = require("alpha.fortune")
+  local align = require("plenary.strings").align_str
   dashboard.section.header.val = {
-    [[                               __                ]],
-    [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-    [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-    [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-    [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-    [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+    [[  ` : | | | |:  ||  :     `  :  |  |+|: | : : :|   .        `              .]],
+    [[      ` : | :|  ||  |:  :    `  |  | :| : | : |:   |  .                    :]],
+    [[         .' ':  ||  |:  |  '       ` || | : | |: : |   .  `           .   :.]],
+    [[                `'  ||  |  ' |   *    ` : | | :| |*|  :   :               :|]],
+    [[        *    *       `  |  : :  |  .      ` ' :| | :| . : :         *   :.||]],
+    [[             .`            | |  |  : .:|       ` | || | : |: |          | ||]],
+    [[      '          .         + `  |  :  .: .         '| | : :| :    .   |:| ||]],
+    [[         .                 .    ` *|  || :       `    | | :| | :      |:| |]],
+    [[ .                .          .        || |.: *          | || : :     :|||]],
+    [[        .            .   . *    .   .  ` |||.  +        + '| |||  .  ||`]],
+    [[     .             *              .     +:`|!             . ||||  :.||`]],
+    [[ +                      .                ..!|*          . | :`||+ |||`]],
+    [[     .                         +      : |||`        .| :| | | |.| ||`     .]],
+    [[       *     +   '               +  :|| |`     :.+. || || | |:`|| `]],
+    [[                            .      .||` .    ..|| | |: '` `| | |`  +]],
+    [[  .       +++                      ||        !|!: `       :| |]],
+    [[              +         .      .    | .      `|||.:      .||    .      .    `]],
+    [[          '                           `|.   .  `:|||   + ||'     `]],
+    [[  __    +      *                         `'       `'|.    `:]],
+    [["'  `---"""----....____,..^---`^``----.,.___          `.    `.  .    ____,.,-]],
+    [[    ___,--'""`---"'   ^  ^ ^        ^       """'---,..___ __,..---""']],
+    [[--"'                           ^                         ``--..,__]],
   }
 
   dashboard.section.buttons.val = {
@@ -48,22 +66,46 @@ local function config()
     return results
   end
 
+  ---@param lines table<string>
+  ---@param min_width integer?
+  ---@return table<string>
+  local function center(lines, min_width)
+    local max_len = min_width or 0
+    for _, line in ipairs(lines) do
+      max_len = math.max(max_len, #line)
+    end
+    local result = {}
+    for _, line in ipairs(lines) do
+      local diff = max_len - #line
+      local left_pad = math.floor(diff / 2)
+      result[#result + 1] = align((align(line, #line + left_pad)), max_len, true)
+    end
+    return result
+  end
+
   local function footer()
+    local width = 54
     local plugin_count = 0
     for _, _ in pairs(packer_plugins) do
       plugin_count = plugin_count + 1
     end
-    return zip_map({ format({ plugin_count }), id }, {
-      "     %d plugins installed     ",
-      "Neovim is pretty cool actually",
-    })
+    return center(vim.tbl_flatten({
+      center({
+        "",
+        string.format("%d plugins installed", plugin_count),
+        "Neovim is pretty cool actually",
+        "",
+        "",
+      }, width),
+      fortune(width),
+    }))
   end
 
   dashboard.section.footer.val = footer()
 
   dashboard.section.footer.opts.hl = "Type"
   dashboard.section.header.opts.hl = "Include"
-  dashboard.section.buttons.opts.hl = "Keyword"
+  dashboard.section.buttons.opts.hl = "Identifier"
 
   dashboard.opts.opts.noautocmd = true
   require("alpha").setup(dashboard.opts)
