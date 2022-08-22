b333e393b1754afc58ed3538abf948f1b001b8af
Elizabeth Paź <me@ehllie.xyz>
2022-08-22 16:24:05 +0200

    Prettier alpha config

--- user/nvim/lua/plug-conf/alpha.lua
+++ user/nvim/lua/plug-conf/alpha.lua
@@ -2,7 +2,7 @@ local function config()
   local dashboard = require("alpha.themes.dashboard")
   local fortune = require("alpha.fortune")
   local center = require("local-lib").center
-  dashboard.section.header.val = {
+  local header = {
     [[  ` : | | | |:  ||  :     `  :  |  |+|: | : : :|   .        `              .]],
     [[      ` : | :|  ||  |:  :    `  |  | :| : | : |:   |  .                    :]],
     [[         .' ':  ||  |:  |  '       ` || | : | |: : |   .  `           .   :.]],
@@ -27,7 +27,7 @@ local function config()
     [[--"'                           ^                         ``--..,__]],
   }
 
-  dashboard.section.buttons.val = {
+  local buttons = {
     dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
     dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
     dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
@@ -55,11 +55,13 @@ local function config()
     }))
   end
 
+  dashboard.section.header.val = header
+  dashboard.section.header.opts.hl = "Type"
+  dashboard.section.buttons.val = buttons
+  dashboard.section.buttons.opts.hl = "Keyword"
   ---@diagnostic disable-next-line
   dashboard.section.footer.val = footer()
   dashboard.section.footer.opts.hl = "Identifier"
-  dashboard.section.header.opts.hl = "Type"
-  dashboard.section.buttons.opts.hl = "Keyword"
 
   dashboard.opts.opts.noautocmd = true
   require("alpha").setup(dashboard.opts)
