4026c144f2ebfddbad760ba449a9626cdcb14ac7
Elizabeth Paź <me@ehllie.xyz>
2022-09-28 10:03:27 +0200

    Added cachix to list of trusted substituters

--- default.nix
+++ default.nix
@@ -70,6 +70,12 @@ let
     nix = {
       extraOptions = "experimental-features = nix-command flakes";
 
+      settings = {
+        trusted-substituters = [ https://nix-community.cachix.org ];
+        extra-substituters = [ https://nix-community.cachix.org ];
+        extra-trusted-public-keys = [ nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= ];
+      };
+
       gc = {
         automatic = true;
         dates = "daily";
