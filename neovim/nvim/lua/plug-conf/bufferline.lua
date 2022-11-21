a0bea1fb5b39bb9b03b2b6686aa3c168192621a1
Elizabeth Paź <me@ehllie.xyz>
2022-11-21 14:35:43 +0100

    Configure buffer keybinds

--- neovim/nvim/lua/plug-conf/bufferline.lua
+++ neovim/nvim/lua/plug-conf/bufferline.lua
@@ -1,5 +1,40 @@
 local function config()
-  require("bufferline").setup()
+  local bline = require("bufferline")
+  bline.setup()
+
+  require("which-key").register({
+    ["<leader>q"] = {
+      h = {
+        function()
+          bline.close_in_direction("left")
+        end,
+        "Close buffers to the left",
+      },
+      l = {
+        function()
+          bline.close_in_direction("right")
+        end,
+        "Close buffers to the right",
+      },
+    },
+    ["<C-S-h>"] = {
+      function()
+        bline.move(-1)
+      end,
+      "Move buffer to the left",
+    },
+    ["<C-S-l>"] = {
+      function()
+        bline.move(1)
+      end,
+      "Move buffer to the right",
+    },
+  })
 end
 
-return { "akinsho/bufferline.nvim", config = config, tag = "v3.*" }
+return {
+  "akinsho/bufferline.nvim",
+  config = config,
+  tag = "v3.*",
+  requires = "folke/which-key.nvim",
+}
