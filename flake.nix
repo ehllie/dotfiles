35d6612b3f86ddbc378d5c577d6bce972b6190d8
Elizabeth Paź <me@ehllie.xyz>
2023-03-07 08:57:52 +0100

    Smaller more explicit flake.nix

--- flake.nix
+++ flake.nix
@@ -28,86 +28,15 @@
     , docs-gen
     }:
     let
-      defaultConfig = rec {
-        userName = "ellie";
-        userDesc = "Elizabeth";
-        hardware = null;
-        dotfileRepo = "github:ehllie/dotfiles";
-        repoUrl = "https://github.com/ehllie/dotfiles.git";
-        homeDir = "/home/" + userName;
-        repoDir = homeDir + "/Code/dotfiles";
-        shell = "zsh";
-        samba = false;
-        fontsize = 8;
-        colourscheme.catppuccin.enable = true;
-        hmMod = true;
-      };
-
-      utils = import ./utils.nix { inherit nixpkgs nixpkgs-darwin home-manager darwin; };
-      inherit (utils) flattenConfigs mkOutputs;
-      inherit (nixpkgs.lib) recursiveUpdate;
-
-      flakeConfig = args@{ dfconf, homeExtra ? [ ], nixosExtra ? [ ], darwinExtra ? [ ], ... }:
-        let
-          overlays = import ./overlays [
-            taffybar.overlays
-            # nil.overlays.default
-            (_: _: { inherit (nil.packages.${dfconf.system}) nil; })
-            ante.overlays.default
-            docs-gen.overlays.default
-          ];
-          finalConf = recursiveUpdate defaultConfig dfconf;
-          hmExtra =
-            if finalConf.hmMod
-            then { home-manager = { useGlobalPkgs = true; useUserPackages = true; }; }
-            else { };
-        in
-        mkOutputs (args // {
-          dfconf = finalConf;
-          homeExtra = homeExtra ++ [
-            overlays
-          ];
-          nixosExtra = nixosExtra ++ [
-            overlays
-            hmExtra
-          ];
-          darwinExtra = darwinExtra ++ [
-            overlays
-            hmExtra
-          ];
-          root = ./.;
-        });
+      inherit (nixpkgs.lib) genAttrs;
+      inherit (builtins) attrNames;
+      enumerateHosts = genAttrs (attrNames self.nixosConfigurations ++ attrNames self.darwinConfigurations);
     in
-    flattenConfigs [
-      (flakeConfig {
-        dfconf = {
-          system = "x86_64-linux";
-          hostName = "nixgram";
-          hardware = "dell-gram";
-          windowManager = "xmonad";
-          samba = true;
-          graphical = true;
-        };
-      })
-      (flakeConfig {
-        dfconf = {
-          system = "x86_64-linux";
-          hostName = "nixdesk";
-          hardware = "desktop";
-          windowManager = "xmonad";
-          fontsize = 11;
-          graphical = true;
-        };
-      })
-      (flakeConfig {
-        dfconf = rec {
-          system = "aarch64-darwin";
-          hostName = "EllMBP";
-          homeDir = "/Users/ellie";
-          repoDir = homeDir + "/Code/dotfiles";
-          graphical = false;
-          fontsize = 13;
-        };
-      })
-    ];
+    {
+      homeConfigurations = enumerateHosts (host: {
+        "ellie@${host}" = { };
+      });
+      nixosConfigurations = { };
+      darwinConfigurations = { };
+    };
 }
