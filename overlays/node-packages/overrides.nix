f8ebdccc0b15e1e9ee95b45f6102138b7eb26c69
Elizabeth Paź <me@ehllie.xyz>
2023-04-20 13:01:11 +0200

    Update node-packages

--- overlays/node-packages/overrides.nix
+++ overlays/node-packages/overrides.nix
@@ -2,6 +2,8 @@ self: super: {
   volar = self."@volar/vue-language-server".override {
     name = "volar";
   };
-  tailwindcss-language-server = self."@tailwindcss/language-server".override
-    { name = "tailwindcss-language-server"; };
+
+  tailwindcss-language-server = self."@tailwindcss/language-server".override {
+    name = "tailwindcss-language-server";
+  };
 }
