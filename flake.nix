7e8546a5c09f77cc008eb5c9974481a32098d900
Elizabeth Paź <me@ehllie.xyz>
2023-06-27 19:08:31 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     ante = {
