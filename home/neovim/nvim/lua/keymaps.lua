af90e4064512ff1cfa4da23bb210098d550754ab
Elizabeth Paź <me@ehllie.xyz>
2023-08-22 19:12:17 +0200

    Fix MultiMacro keybind

--- home/neovim/nvim/lua/keymaps.lua
+++ home/neovim/nvim/lua/keymaps.lua
@@ -42,7 +42,7 @@ end
 
 vim.api.nvim_create_user_command("MultiMacro", function(arg_tab)
   local reg_name = vim.fn.getcharstr()
-  pcall(vim.cmd, arg_tab.line1 .. "," .. arg_tab.line2 .. "g/^/norm @" .. reg_name)
+  pcall(vim.cmd, arg_tab.line1 .. "," .. arg_tab.line2 .. "g/^/norm! @" .. reg_name)
   vim.cmd("nohlsearch")
 end, { nargs = 0, range = true })
 
@@ -96,7 +96,7 @@ register({
   p = { [["_dP]], "Paste" }, -- Doesn't overwrite the clipboard with the replaced text
   ["<"] = { "<gv", "Reduce indent" },
   [">"] = { ">gv", "Increase indent" },
-  ["@"] = { "<cmd>MultiMacro<CR>", "Run macro on selected lines" },
+  ["@"] = { ":MultiMacro<CR>", "Run macro on selected lines" },
 }, { mode = "v" })
 
 -- Command --
