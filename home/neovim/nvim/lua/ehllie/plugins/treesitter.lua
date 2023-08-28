b89d5aa942b90bb3e6e86f5375872d246525ea78
Elizabeth Paź <me@ehllie.xyz>
2023-08-28 12:44:28 +0200

    Uninstall treesitter parsers when gcc changes

--- home/neovim/nvim/lua/ehllie/plugins/treesitter.lua
+++ home/neovim/nvim/lua/ehllie/plugins/treesitter.lua
@@ -1,6 +1,26 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  main = "nvim-treesitter.configs",
+  config = function(_, opts)
+    local file_path = vim.fn.stdpath("state") .. "/prev_gcc"
+    local prev_gcc = ""
+    local file = io.open(file_path, "r")
+    if file then
+      prev_gcc = file:read("*a")
+      file:close()
+    end
+    if prev_gcc ~= store.gcc then
+      vim.cmd("TSUninstall all")
+      file = io.open(file_path, "w")
+      if file then
+        file:write(store.gcc)
+        file:close()
+      else
+        print("Could not open file: " .. file_path)
+      end
+    end
+
+    require("nvim-treesitter.configs").setup(opts)
+  end,
   opts = {
     ensure_installed = "all",
     highlight = {
