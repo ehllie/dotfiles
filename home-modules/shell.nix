e88fa9a263146c6ced195a09f6e70389e5509ce0
Elizabeth Paź <me@ehllie.xyz>
2023-12-06 12:41:29 +0100

    Theme tmux

--- home-modules/shell.nix
+++ home-modules/shell.nix
@@ -163,6 +163,46 @@ in
               set -g @yank_action 'copy-pipe'
             '';
           }
+          {
+            plugin = tmuxPlugins.catppuccin.overrideAttrs (_: {
+              version = "unstable-2023-11-01";
+              src = pkgs.fetchFromGitHub {
+                owner = "catppuccin";
+                repo = "tmux";
+                rev = "47e33044b4b47b1c1faca1e42508fc92be12131a";
+                hash = "sha256-kn3kf7eiiwXj57tgA7fs5N2+B2r441OtBlM8IBBLl4I=";
+              };
+            });
+            extraConfig = ''
+              set -g @catppuccin_flavour 'frappe'
+
+              set -g @catppuccin_window_left_separator ""
+              set -g @catppuccin_window_right_separator " "
+              set -g @catppuccin_window_middle_separator " █"
+              set -g @catppuccin_window_number_position "right"
+
+              set -g @catppuccin_window_default_fill "number"
+              set -g @catppuccin_window_default_text "#W"
+
+              set -g @catppuccin_window_current_fill "number"
+              set -g @catppuccin_window_current_text "#W"
+
+              set -g @catppuccin_status_modules_right "session date_time"
+              set -g @catppuccin_status_left_separator  " "
+              set -g @catppuccin_status_right_separator ""
+              set -g @catppuccin_status_right_separator_inverse "no"
+              set -g @catppuccin_status_fill "icon"
+              set -g @catppuccin_status_connect_separator "no"
+
+              set -g @catppuccin_date_time_text "%a %-d %b %H:%M"
+            '';
+          }
+          {
+            plugin = tmuxPlugins.tmux-fzf;
+            extraConfig = ''
+
+            '';
+          }
         ];
       };
   };
