5f14c594b45646fe24a083a61821a149e4c2b19e
Elizabeth Paź <me@ehllie.xyz>
2024-01-17 11:16:11 +0100

    Switch to rustaceanvim

--- home-modules/neovim/nvim/lua/ehllie/plugins/lsp/settings/rust_analyzer.lua
+++ home-modules/neovim/nvim/lua/ehllie/plugins/lsp/settings/rust_analyzer.lua
@@ -1,37 +1,49 @@
 return {
   extended = {
-    "simrat39/rust-tools.nvim",
+
+    "mrcjkb/rustaceanvim",
+    version = "^3",
+    ft = { "rust" },
     config = function()
-      local rt = require("rust-tools")
       local handlers = require("ehllie.plugins.lsp.handlers")
 
-      local opts = {
-        on_attach = handlers.make_on_attach(function()
-          return {
-            ["<leader>l"] = {
-              a = { rt.code_action_group.code_action_group, "Rust code actions" },
-              m = { rt.expand_macro.expand_macro, "Expand macro" },
-            },
-            ["K"] = { rt.hover_actions.hover_actions, "Rust hover actions" },
-          }
-        end),
-        capabilities = handlers.mk_capabilities(),
-      }
+      local on_attach = handlers.make_on_attach(function()
+        return {
+          ["<leader>l"] = {
+            a = { "RustLsp codeAction", "Rust code actions" },
+            m = { "RustLsp expandMacro", "Expand macro" },
+          },
+          ["K"] = { "RustLsp hover actions", "Rust hover actions" },
+        }
+      end)
 
-      rt.setup({
+      vim.g.rustaceanvim = {
         server = {
-          on_attach = opts.on_attach,
-          capabilities = opts.capabilities,
-
+          on_attach = on_attach,
           settings = {
             ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                runBuildScripts = true,
+              },
               checkOnSave = {
+                allFeatures = true,
                 command = "clippy",
+                extraArgs = { "--no-deps" },
+              },
+              procMacro = {
+                enable = true,
+                ignored = {
+                  ["async-trait"] = { "async_trait" },
+                  ["napi-derive"] = { "napi" },
+                  ["async-recursion"] = { "async_recursion" },
+                },
               },
             },
           },
         },
-      })
+      }
     end,
   },
 }
