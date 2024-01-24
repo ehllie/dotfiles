31a6c23ae365f5b33201a6d7f2d8984326a2d391
Elizabeth Paź <me@ehllie.xyz>
2024-01-24 13:33:03 +0100

    Change flash keymaps

--- home-modules/neovim/nvim/lua/ehllie/plugins/flash.lua
+++ home-modules/neovim/nvim/lua/ehllie/plugins/flash.lua
@@ -4,7 +4,7 @@ return {
   opts = {},
   keys = {
     {
-      "s",
+      "<leader>w",
       mode = { "n", "x", "o" },
       function()
         require("flash").jump()
@@ -12,7 +12,7 @@ return {
       desc = "Flash",
     },
     {
-      "S",
+      "<leader>W",
       mode = { "n", "x", "o" },
       function()
         require("flash").treesitter()
