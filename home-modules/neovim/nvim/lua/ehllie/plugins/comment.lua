3e9dca79e7168708633b671c37956b47a7114174
Elizabeth Paź <me@ehllie.xyz>
2024-09-06 15:31:10 +0200

    Remove ts-context-commentstring

--- home-modules/neovim/nvim/lua/ehllie/plugins/comment.lua
+++ home-modules/neovim/nvim/lua/ehllie/plugins/comment.lua
@@ -1,7 +1,6 @@
 return {
   "numToStr/Comment.nvim",
   config = function()
-    local util = require("Comment.utils")
     require("Comment").setup({
       ignore = "^$",
       toggler = {
@@ -17,25 +16,9 @@ return {
         extra = true,
         block = true,
       },
-      pre_hook = function(ctx)
-        if vim.bo.filetype == "typescript" then
-          local location = nil
-          if ctx.ctype == util.ctype.blockwise then
-            location = require("ts_context_commentstring.utils").get_cursor_location()
-          elseif ctx.cmotion == util.cmotion.v or ctx.cmotion == util.cmotion.V then
-            location = require("ts_context_commentstring.utils").get_visual_start_location()
-          end
-
-          return require("ts_context_commentstring.internal").calculate_commentstring({
-            key = ctx.ctype == util.ctype.linewise and "__default" or "__multiline",
-            location = location,
-          })
-        end
-      end,
     })
   end,
   dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
     "folke/which-key.nvim",
   },
 }
